241aaa50f652537e847e276188f3ef7cf87efac9
Kieran Herr <kieranherr@gmail.com>
2020-07-01 16:02:56 -0500

    Init

---
+++ Sweepstakes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sweepstakes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
