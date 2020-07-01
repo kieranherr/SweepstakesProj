1f84b026797ca5d7076920c785529fb03d14d287
Kieran Herr <kieranherr@gmail.com>
2020-07-01 16:35:38 -0500

    Added the rest of the user story methods

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -13,6 +13,18 @@ namespace Sweepstakes
         {
             sweepName = name;
         }
-     
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        public Contestant PickWinner()
+        {
+            Contestant contestant = new Contestant();
+            return contestant; 
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
     }
 }
