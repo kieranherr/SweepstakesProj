1a8ce3dc446f51ac0cf40eeedfa58e2ca5dee28c
Kieran Herr <kieranherr@gmail.com>
2020-07-01 16:07:31 -0500

    Added the sweepstakes and contestant classes

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweepstakes
+{
+    
+    class Sweepstakes
+    {
+        public Sweepstakes()
+        {
+
+        }
+    }
+}
